758d02165510f6b919af7f6a24f5530121bf1efb
Matthew Mantle <memantle@googlemail.com>
2020-08-26 10:39:26 +0100

    Initial commit

--- winners.php
+++ winners.php
@@ -1,26 +1,7 @@
 <?php
 $correct_answers=["Brazil","Argentina","England","Uruguay"];
-$winners;
-$errors=false;
-$msg="";
-if(isset($_POST["submit_btn"])){
-    if(isset($_POST["winners"])){
-    	$winners=$_POST["winners"];
-    	sort($winners);
-		sort($correct_answers);
-		if($winners===$correct_answers){
-			$msg.="<p>Well done. You are correct.</p>";
-		}else{
-			$msg.="<p>Wrong answer.</p>";
-		}
-    }else{
-    	$errors=true;
-		$msg.="<p>You didn't select any teams.</p>";
-    }
-}else{
-	$errors=true;
-	$msg.="<p>You shouldn't have got to this page</p>";
-}
+$winners=$_POST["winners"];
+print_r($winners);
 
 ?>
 <!DOCTYPE html>
@@ -30,20 +11,6 @@ if(isset($_POST["submit_btn"])){
 </head>
 <body>
 <h1>World Cup Winners</h1>
-<?php
-if($errors){
-	echo $msg;
-}else{
-	echo $msg;
-	echo "<p>You answered</p>";
-	echo "<ul>";
-	foreach($winners as $winner)
-	{
-		echo "<li>".$winner."</li>";
-	}
-	echo "</ul>";
-}
-?>
 
 </body>
 </html>
